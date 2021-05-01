d3279b4451298692863cfd9909a7f5ee33d2c7a8
Hyeontae <dchs504@gmail.com>
2021-05-01 12:35:18 +0900

    Add download URL

--- download_processing_data.sh
+++ download_processing_data.sh
@@ -1,2 +1,24 @@
-wget --load-cookies ~/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ~/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1M5U1saofFTQIwIYI1Sa44NkP0fTsswE8' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1M5U1saofFTQIwIYI1Sa44NkP0fTsswE8" -O ./data/DrRepair_deepfix.tar && rm -rf ~/cookies.txt
-tar -xvf data/DrRepair_deepfix.tar
+echo="DeepFix raw data download.."
+file_id="1GqphtPgxbkaq_tgCtYn4iwpt1vZNdyLL"
+file_name="data/deepfix_raw_data/dataset.db"
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${file_id}" -o ${file_name}
+
+echo="DeepFix preprocessed data download"
+file_id="1DbbIouhqu8ZAanywm956ZQEYqbJmRBdl"
+file_name="data/DeepFix.tar"
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${file_id}" -o ${file_name}
+tar -xvf data/DeepFix.tar -C ./data
+rm data/DeepFix.tar
+
+#echo="DrRepair_deepfix preprocessed data download"
+#file_id="1bHUcj8C8fhioRx2g2f8fNwwIM1QNpRXj"
+#file_name="data/DrRepair_deepfix.tar"
+#curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
+#code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+#curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${file_id}" -o ${file_name}
+#tar -xvf data/DrRepair_deepfix.tar -C ./data
+#rm data/DrRepair_deepfix.tar
